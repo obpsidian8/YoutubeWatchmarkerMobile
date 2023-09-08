5d5ffa62332bb8c2eab2aae53caad1403a0ca4f3
SeunO <seun747@gmail.com>
2023-09-08 15:23:38 -0700

    WindowsInterval is set back to 600ms. Lower page checking is failing on mobile browsers

--- youtube.js
+++ youtube.js
@@ -281,7 +281,7 @@ function messageReceivedProcess (objData, objSender, funcResponse)
 // ###########################################################
 // This will set the interval to update the list of elements on the page and also to send time info to backend. Need to be fast beause you scroll the page quickly
 // Updates data from the page at given interval
-window.setInterval(checkPage, 100);
+window.setInterval(checkPage, 600);
 function checkPage()
     {
         display_every = 90
